3d5c5f9c6eff1b04ffa73d1781415e093c56fdc3
ortiwii <ergorka@gmail.com>
2020-10-19 12:26:19 +0200

    zerbKud

---
+++ src/main/java/ehu/isad/db/ZerbitzuKud.java
@@ -0,0 +1,47 @@
+package ehu.isad.db;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZerbitzuKud {
+
+    private static final ZerbitzuKud instance = new ZerbitzuKud();
+
+    public static ZerbitzuKud getInstance() {
+        return instance;
+    }
+
+    private ZerbitzuKud() {
+    }
+    public void KenduZerbitzua (String zerbitzua){
+        String query = "delete from services where izena = '"+zerbitzua+"';";
+        DBKudeatzaile dbKudeatzaile = DBKudeatzaile.getInstantzia();
+        dbKudeatzaile.execSQL(query);
+    }
+    public List<String> lortuZerbitzuak() {
+
+        String query = "select id, izena from services";
+        DBKudeatzaile dbKudeatzaile = DBKudeatzaile.getInstantzia();
+        ResultSet rs = dbKudeatzaile.execSQL(query);
+
+        List<String> emaitza = new ArrayList<>();
+        try {
+            while (rs.next()) {
+
+                int kodea = rs.getInt("id");
+                String izena = rs.getString("izena");
+
+                System.out.println(kodea + ":" + izena);
+                emaitza.add(izena);
+
+            }
+        } catch(SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+        return emaitza;
+    }
+}
+
